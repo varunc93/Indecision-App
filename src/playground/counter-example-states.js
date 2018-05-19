685d6abe1dc27c710909f2891ccf1ebd9ee46a0b
VC93 <reachvc93@gmail.com>
2018-05-18 22:21:39 -0700

    Change counter example

--- src/playground/counter-example-states.js
+++ src/playground/counter-example-states.js
@@ -9,6 +9,23 @@ class Counter extends React.Component{ //state allows the object to be rendered 
         }
     }
 
+    componentDidMount() {
+        try {
+            const count = parseInt(localStorage.getItem("count"),10);
+            if(!isNaN(count)){
+                this.setState(() => ({count}));
+            }
+        }
+        catch (error) {
+
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.count != this.state.count){
+            localStorage.setItem("count", this.state.count );
+        }
+    }
     handleAddOne () {
         this.setState((prevState) =>
             {
